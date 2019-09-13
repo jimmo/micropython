1f1274168475782aa02a30c81c9a9f53482575e3
Jim Mussared <jim.mussared@gmail.com>
2019-09-13 16:11:38 +1000

    IDF 4.x wifi -- fix linking of blobs

--- ports/esp32/main.c
+++ ports/esp32/main.c
@@ -150,7 +150,11 @@ soft_reset:
 }
 
 void app_main(void) {
-    nvs_flash_init();
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      nvs_flash_erase();
+      nvs_flash_init();
+    }
     xTaskCreatePinnedToCore(mp_task, "mp_task", MP_TASK_STACK_LEN, NULL, MP_TASK_PRIORITY, &mp_main_task_handle, MP_TASK_COREID);
 }
 
