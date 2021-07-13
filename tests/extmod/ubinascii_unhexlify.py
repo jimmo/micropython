db6c9b090843452f1b7f31109126b88b66738a45
Jim Mussared <jim.mussared@gmail.com>
2021-07-13 22:49:49 +1000

    tests/extmod/ubinascii: Add tests for bytes.hex etc.
    Also make the sep test not micropython-specific.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- tests/extmod/ubinascii_unhexlify.py
+++ tests/extmod/ubinascii_unhexlify.py
@@ -7,10 +7,14 @@ except ImportError:
     print("SKIP")
     raise SystemExit
 
-print(binascii.unhexlify(b"0001020304050607"))
-print(binascii.unhexlify(b"08090a0b0c0d0e0f"))
-print(binascii.unhexlify(b"7f80ff"))
-print(binascii.unhexlify(b"313233344142434461626364"))
+for x in (
+    b"0001020304050607",
+    b"08090a0b0c0d0e0f",
+    b"7f80ff",
+    b"313233344142434461626364",
+):
+    print(binascii.unhexlify(x))
+    print(bytes.fromhex(str(x, "utf-8")))
 
 try:
     a = binascii.unhexlify(b"0")  # odd buffer length
