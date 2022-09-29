e153658e85ee7ddf4f2945d89604b181b530c516
robert-hh <robert@hammelrath.com>
2022-09-29 16:13:23 +0200

    samd/clock_config: Extend the SAMD51 us-counter to 60 bit.
    - Removing the difference in the time.ticks_us() range between
      SAMD21 and SAMD51.
    - Adds the function mp_hal_ticks_us_64() and use it for:
       + SAMD51's mp_hal_ticks_us and mp_hal_delay_us().
         For SAMD21, keep the previous methods, which are faster.
       + mp_hal_ticks_ms() and mp_hal_tick_ms_64(), which saves some bytes
         and removes a potential race condition every 50 days.
    - Set the us-counter for SAMD51 to 16 MHz for a faster reading
      of the us value.
    
    Note: With SAMD51, mp_hal_ticks_us_64() has a 60 bit range only,
    which is still a long time (~36000 years).

--- ports/samd/samd_soc.c
+++ ports/samd/samd_soc.c
@@ -64,7 +64,7 @@ static void usb_init(void) {
     tusb_init();
 }
 
-// Initialize the microsecond counter on TC 0/1
+// Initialize the µs counter on TC 0/1 or TC4/5
 void init_us_counter(void) {
     #if defined(MCU_SAMD21)
 
@@ -85,6 +85,9 @@ void init_us_counter(void) {
     TC4->COUNT32.READREQ.reg = TC_READREQ_RREQ | TC_READREQ_RCONT | 0x10;
     while (TC4->COUNT32.STATUS.bit.SYNCBUSY) {
     }
+    // Enable the IRQ
+    TC4->COUNT32.INTENSET.reg = TC_INTENSET_OVF;
+    NVIC_EnableIRQ(TC4_IRQn);
 
     #elif defined(MCU_SAMD51)
 
@@ -103,6 +106,9 @@ void init_us_counter(void) {
     while (TC0->COUNT32.SYNCBUSY.bit.ENABLE) {
     }
 
+    // Enable the IRQ
+    TC0->COUNT32.INTENSET.reg = TC_INTENSET_OVF;
+    NVIC_EnableIRQ(TC0_IRQn);
     #endif
 }
 
